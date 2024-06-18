2b6cbe00e858eb1dfe1c018bdee9d06bb6aa8202
Kaustubh <83804086+Kaustubh8998@users.noreply.github.com>
2024-06-18 19:40:13 +0530

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import quickMeds from "../src/assets/quickMeds.png";
+import quickMeds from "./src/assets/quickMeds.png";
 import "@fontsource/plus-jakarta-sans";
 
 const SideBar = () => {
